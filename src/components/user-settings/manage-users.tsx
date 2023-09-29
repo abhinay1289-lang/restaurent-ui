db0ed4cade403375e6fc82a462d60e2faa4076b7
keshava katakam <keshava.katakam@peopletech.com>
2023-09-29 18:22:48 +0530

    modified

--- src/components/user-settings/manage-users.tsx
+++ src/components/user-settings/manage-users.tsx
@@ -1,4 +1,13 @@
+import { InputLabel, TextField } from "@mui/material";
+
 const Manageusers = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <InputLabel>Manage-users</InputLabel>
+        <TextField sx></TextField>
+      </div>
+    </div>
+  );
 };
 export default Manageusers;
