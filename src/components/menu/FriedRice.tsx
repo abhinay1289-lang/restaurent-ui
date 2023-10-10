e8d044be3aff9b9de89dfdb520674f663141e649
keshava katakam <keshava.katakam@peopletech.com>
2023-10-10 17:14:23 +0530

    modified

--- src/components/menu/FriedRice.tsx
+++ src/components/menu/FriedRice.tsx
@@ -1,4 +1,23 @@
+import Nonveg from "./Non-veg";
+import Veg from "./Veg";
+
 const FriedRice = () => {
-  return <></>;
+  const biryaniItems: string[] = ["chicken", "kjbhi", "gfdtr", "egr", "jguhe3"];
+
+  return (
+    <div
+      style={{
+        backgroundColor: "transparent",
+        display: "flex",
+        width: "100%",
+        height: "100%",
+        justifyContent: "space-around",
+        marginTop: "10%",
+      }}
+    >
+      <Nonveg items={biryaniItems} />
+      <Veg items={biryaniItems} />
+    </div>
+  );
 };
 export default FriedRice;
--- src/components/menu/Rice.tsx
+++ src/components/menu/Rice.tsx
@@ -1,4 +1,29 @@
+import Nonveg from "./Non-veg";
+import Veg from "./Veg";
+
 const Rice = () => {
-  return <></>;
+  const biryaniItems: string[] = [
+    "djygfhdrtksrje",
+    "kwhedwe",
+    "kshhrdtreifwe",
+    "jhaekgwd",
+    "jhawguhe3",
+  ];
+
+  return (
+    <div
+      style={{
+        backgroundColor: "transparent",
+        display: "flex",
+        width: "100%",
+        height: "100%",
+        justifyContent: "space-around",
+        marginTop: "10%",
+      }}
+    >
+      <Nonveg items={biryaniItems} />
+      <Veg items={biryaniItems} />
+    </div>
+  );
 };
 export default Rice;
